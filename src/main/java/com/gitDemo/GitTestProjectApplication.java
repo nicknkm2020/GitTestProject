458cd268f2716e225e742b644f3e33068d2ee745
naman <namanmandli@gmail.com>
2018-06-07 22:14:23 -0400

    First Commit

---
+++ src/main/java/com/gitDemo/GitTestProjectApplication.java
@@ -0,0 +1,12 @@
+package com.gitDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestProjectApplication.class, args);
+	}
+}
